54d281592993aa61e2102660ddc58518baf33b7f
HienaDev <ant.rodrigues1999@gmail.com>
2023-06-02 13:14:08 +0100

    Create HowMany class

---
+++ HowManyOfThisType/Checker.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HowManyOfThisType
+{
+    public static class Checker
+    {
+        
+
+        public static int HowManyOfType<T>(IEnumerable<object> items)
+        {
+            
+        }
+
+    }
+}
