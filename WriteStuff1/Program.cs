28106c124aa10738f1d5ebc6000b67c2780b3300
HienaDev <ant.rodrigues1999@gmail.com>
2023-06-02 14:52:15 +0100

    Add write stuff program

---
+++ WriteStuff1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace WriteStuff1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Queue<string> texto = new Queue<string>();
+
+            string input = null;
+
+            while (input != "")
+            {
+                input = Console.ReadLine();
+
+                texto.Enqueue(input);
+            }
+
+            File.WriteAllLines(args[0], texto);
+
+        }
+    }
+}
